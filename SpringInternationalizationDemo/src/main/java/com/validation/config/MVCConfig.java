e586b4cf02a5de55a318a5a5790d9332072a5121
Pavithra-cpu <55425568+Pavithra-cpu@users.noreply.github.com>
2020-02-20 16:05:33 +0530

    SpringMVCInternationalizationDema

---
+++ SpringInternationalizationDemo/src/main/java/com/validation/config/MVCConfig.java
@@ -0,0 +1,61 @@
+package com.validation.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.validation"})
+public class MVCConfig implements WebMvcConfigurer {
+
+	@Bean
+	public InternalResourceViewResolver resolver()
+	{
+		InternalResourceViewResolver resolver =new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource()
+	{
+		ReloadableResourceBundleMessageSource messageSource=new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
+	@Bean
+	public CookieLocaleResolver localeResolver()
+	{
+		CookieLocaleResolver localeResolver=new CookieLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		localeResolver.setCookieName("locale-cookie");
+		localeResolver.setCookieMaxAge(1800);
+		return localeResolver;
+	}
+	@Bean
+	public LocaleChangeInterceptor localeInterceptor()
+	{
+		LocaleChangeInterceptor interceptor=new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		return interceptor;
+	}
+	
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeInterceptor());
+	}
+}
---
+++ SpringInternationalizationDemo/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,11 @@
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring MVC Internationalization</title>
+</head>
+<body>
+<spring:message code="welcome:msg" text="Welcome"></spring:message>
+</body>
+</html>
