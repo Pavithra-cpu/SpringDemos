f1cdaa0378cfdd6a4c701f4ed6fc1bc3b97eb565
Pavithra-cpu <55425568+Pavithra-cpu@users.noreply.github.com>
2020-02-20 10:36:39 +0530

    SpringMVCFormValidationAnnotationBased

---
+++ SpringMVCFormValidation/src/main/java/com/validation/config/SpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,33 @@
+package com.validation.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[]
+				{
+						MVCConfig.class
+				};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[]
+				{
+						"/"
+				};
+	}
+	
+	
+
+}
